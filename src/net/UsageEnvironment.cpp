b3467964dc6478d5b2de71f8595de2d60b68ed49
larrystd <Venray.Kong@outlook.com>
2021-06-23 11:51:34 +0800

    first commit

---
+++ src/net/UsageEnvironment.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include "net/UsageEnvironment.h"
+#include "base/New.h"
+
+UsageEnvironment* UsageEnvironment::createNew(EventScheduler* scheduler, ThreadPool* threadPool)
+{
+    if(!scheduler)
+        return NULL;
+    
+    //return new UsageEnvironment(scheduler, threadPool);
+    return New<UsageEnvironment>::allocate(scheduler, threadPool);
+}
+
+UsageEnvironment::UsageEnvironment(EventScheduler* scheduler, ThreadPool* threadPool) :
+    mScheduler(scheduler),
+    mThreadPool(threadPool)
+{
+
+}
+
+UsageEnvironment::~UsageEnvironment()
+{
+
+}
+
+EventScheduler* UsageEnvironment::scheduler()
+{
+    return mScheduler;
+}
+
+ThreadPool* UsageEnvironment::threadPool()
+{
+    return mThreadPool;
+}
